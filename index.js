ef12272020693202d2962abbbfd029416a107d03
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-31 16:35:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ function iterate(callback) {
   var evens = [0, 2, 4, 6, 8, 10];
   evens.forEach(callback);
   return evens;
-
 }
 
-// 2) index iterate(callback) calls the callback on an internal array, then returns the array:
-//   ReferenceError: iterate is not defined
-//     at Context.<anonymous> (test/index-test.js:22:22)
+function doToArray(array, callback) {
+  array.forEach(callback);
+}
+
 
 // 3) index doToArray(array, callback) uses `callback` as the callback for `array`.forEach:
 //   ReferenceError: doToArray is not defined
