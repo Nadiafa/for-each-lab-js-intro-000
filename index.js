eb068dba05774cddd1c4a659e06e5bd92c25d577
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-31 16:33:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function iterativeLog(array) {
+  array.forEach((element, index) => {console.log(`${index}: ${element}`);
+  });
+}
+
+function iterate(array, callback) {
+  array.forEach(callback);
+
+}
+
+// 2) index iterate(callback) calls the callback on an internal array, then returns the array:
+//   ReferenceError: iterate is not defined
+//     at Context.<anonymous> (test/index-test.js:22:22)
+
+// 3) index doToArray(array, callback) uses `callback` as the callback for `array`.forEach:
+//   ReferenceError: doToArray is not defined
+//     at Context.<anonymous> (test/index-test.js:41:7)
+      
+      
+// function doToElementsInArray(array, callback) {
+//   array.forEach(callback);
+// }
+
+// // Add your changeCompletely() function here:
+
+// function changeCompletely(element, index, array) {
+//   array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+// }
